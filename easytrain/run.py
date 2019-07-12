1f477e8bf5d88f5ae446ecd153fe8ab551fa7629
Shuhei <sh7916@gmail.com>
2019-07-12 17:10:59 +0900

    v0.1.0

---
+++ easytrain/run.py
@@ -0,0 +1,43 @@
+
+import json
+from datetime import datetime
+import os
+from os.path import join
+
+import numpy as np
+
+from .train import train_split
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+def start(iterable, name=None, out_dir=None, tqdm=None):
+    if name is None:
+        name = datetime.now().isoformat()
+    if out_dir is None:
+        out_dir = os.getcwd()
+
+    path = join(out_dir, name)
+    os.mkdir(path)
+
+    for train_name, data, target, model, train_params in tqdm(iterable):
+        train_path = join(path, train_name)
+        os.mkdir(train_path)
+
+        for i, (model, fit_history) \
+                in enumerate(train_split(model, data, target,
+                                         **train_params, tqdm=tqdm)):
+            split_path = join(train_path, 'split{:03d}'.format(i))
+            model.save(split_path + '.h5', overwrite=False)
+            with open(split_path + '.json', 'x') as f:
+                json.dump(fit_history, f, cls=MyEncoder, indent=4)
---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+metadata = {
+    'name': 'easytrain',
+    'packages': find_packages(),
+    'version': '0.1.0',
+    'description': 'machine learning utility for lazy man',
+    'python_requires': '>=3.5',
+    'author': 'Shuhei Hirata',
+    'author_email': 'sh7916@gmail.com',
+    'license': 'MIT',
+    'url': 'https://github.com/hrtshu/easytrain.git',
+    'install_requires': [
+        'numpy',
+        'scikit-learn>=0.21.0',
+        'keras>=2.0.0',
+    ],
+}
+
+setup(**metadata)
